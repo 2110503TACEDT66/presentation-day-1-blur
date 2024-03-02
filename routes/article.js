7fabd7ac198fad23f49b5d47b561490704edbe77
BasilBaha <145102981+BasilBaha@users.noreply.github.com>
2024-03-02 23:42:30 +0700

    article v3

--- routes/article.js
+++ routes/article.js
@@ -8,13 +8,14 @@ const {
 } = require("../controllers/article");
 
 const router = express.Router();
+const {protect, authorize} = require('../middleware/auth');
 
-router.route("/").get(getArticles).post(createArticle);
+router.route("/").get(getArticles).post(protect, authorize('admin'), createArticle);
 
 router
   .route("/:id")
   .get(getArticle)
-  .put(updateArticle)
-  .delete(deleteArticle);
+  .put(protect, authorize('admin'),updateArticle)
+  .delete(protect, authorize('admin'),deleteArticle);
 
 module.exports = router;
