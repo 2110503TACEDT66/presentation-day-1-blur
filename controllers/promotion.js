f6c0306c53eb1ab21d167eeb0e2e66fa54df5b34
Korakot Plodjuan <korakot.ptt@gmail.com>
2024-03-03 17:10:51 +0700

    edit-promotion

--- controllers/promotion.js
+++ controllers/promotion.js
@@ -29,3 +29,50 @@ exports.getPromotion = async (req, res, next) => {
     res.status(400).json({ success: false });
   }
 };
+exports.createPromotion = async (req, res, next) => {
+  const promotion = await Promotion.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: promotion
+  });
+};
+
+// @desc    Update promotion
+// @route   PUT /api/v1/promotions/:id
+// @access  Private
+exports.updatePromotion = async (req, res, next) => {
+  try {
+    const promotion = await Promotion.findByIdAndUpdate(req.params.id, req.body, {
+      new : true,
+      runValidators: true
+    });
+
+    if(!promotion){
+      return res.status(400).json({success:false});
+
+    }
+    res.status(200).json({success:true, data : promotion});
+
+  } catch (error) {
+    res.status(400).json({success:false});
+  }
+
+};
+
+// @desc    Delete promotion
+// @route   DELETE /api/v1/promotions/:id
+// @access  Private
+exports.deletePromotion = async (req, res, next) => {
+  try {
+    const promotion = await Promotion.findByIdAndDelete(req.params.id);
+
+    if(!promotion){
+      return res.status(400).json({success: false});
+    }
+
+    res.status(200).json({success: true, data : {}});
+
+  } catch (error) {
+    res.status(400).json({success:false});
+  }
+};
--- routes/promotions.js
+++ routes/promotions.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getPromotions, getPromotion } = require("../controllers/promotion");
+const { getPromotions, getPromotion, createPromotion, updatePromotion, deletePromotion } = require("../controllers/promotion");
 
 const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
 
-router.route("/").get(getPromotions);
+router.route("/").get(getPromotions).post(protect,authorize('admin'),createPromotion);
 
-router.route("/:id").get(getPromotion);
+router.route("/:id").get(getPromotion).put(protect,authorize('admin'),updatePromotion).delete(protect,authorize('admin'),deletePromotion);
 module.exports = router;
