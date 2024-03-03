f50b066b9c7ad53da718947f46c27a2b5bfbfb58
Korakot Plodjuan <korakot.ptt@gmail.com>
2024-03-03 18:12:41 +0700

    FINAL

--- routes/payment.js
+++ routes/payment.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getPayments, getPayment } = require("../controllers/payment");
+const { getPayments, getPayment, createPayment, updatePayment, deletePayment } = require("../controllers/payment");
 
 const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
 
-router.route("/").get(getPayments);
+router.route("/").get(protect,authorize('admin'),getPayments).post(protect,authorize('admin'),createPayment);
 
-router.route("/:id").get(getPayment);
+router.route("/:id").get(protect,authorize('admin'),getPayment).put(protect,authorize('admin'),updatePayment).delete(protect,authorize('admin'),deletePayment);
 module.exports = router;
